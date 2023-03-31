af8fc620713fbed65d5bdc50eceb483bc0c9b872
klemnic <nicholas.klemm@cuw.edu>
2023-03-31 13:06:06 -0500

    Through Fri3.31

--- Dungeon Crawler/Assets/Scripts/DungeonSetUp.cs
+++ Dungeon Crawler/Assets/Scripts/DungeonSetUp.cs
@@ -9,50 +9,16 @@ public class DungeonSetUp : MonoBehaviour
     public GameObject eastExit;
     public GameObject westExit;
 
-    public bool northOn = false;
-    public bool southOn = true;
-    public bool eastOn = true;
-    public bool westOn = true;
+    public bool northOn;
+    public bool southOn;
+    public bool eastOn;
+    public bool westOn;
 
     // Start is called before the first frame update
     void Start()
     {
         //hw answer should be in here!
-        if (northOn = true)
-        {
-            northExit.SetActive(true);
-        }
-        else
-        {
-            northExit.SetActive(false);
-        }
-
-        if (southOn = true)
-        {
-            southExit.SetActive(true);
-        }
-        else
-        {
-            southExit.SetActive(false);
-        }
-
-        if (eastOn = true)
-        {
-            eastExit.SetActive(true);
-        }
-        else
-        {
-            eastExit.SetActive(false);
-        }
-
-        if (westOn = true)
-        {
-            westExit.SetActive(true);
-        }
-        else
-        {
-            westExit.SetActive(false);
-        }
+        
     }
 
     // Update is called once per frame
