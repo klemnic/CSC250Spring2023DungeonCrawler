e1747f5333013084c4915978d251f8fbb23ee8d3
klemnic <nicholas.klemm@cuw.edu>
2023-05-04 20:18:50 -0500

    Final HW

--- Dungeon Crawler/Assets/Scripts/MasterData.cs
+++ Dungeon Crawler/Assets/Scripts/MasterData.cs
@@ -6,30 +6,6 @@ public class MasterData
 {
     public static int count = 0;
     public static string whereDidIComeFrom = "?";
+    public static bool isExiting = true;
 
-    /*public void rollPlayer(string whereFrom)
-    {
-        this.whereFrom = whereDidIComeFrom;
-        {
-            if(whereFrom.Equals("north"))
-            {
-                Transform.rb.northExit;
-            }
-            if (whereFrom.Equals("south"))
-            {
-                Transform.rb.northExit;
-            }
-            if (whereFrom.Equals("east"))
-            {
-                Transform.rb.northExit;
-            }
-            if (whereFrom.Equals("west"))
-            {
-                Transform.rb.northExit;
-            }
-
-        }
-
-
-    }*/
 }
--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -8,16 +8,45 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
+    public GameObject northStart, southStart, eastStart, westStart;
     public float movementSpeed = 40.0f;
-    public string PlayerDirection;
     public bool isMoving;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        this.rb = this.GetComponent<Rigidbody>();
-        print(MasterData.count);
+        //we give ourselves the rigidbody because the rigidbody is what we are referring to
         this.isMoving = false;
+        this.rb = this.GetComponent<Rigidbody>();
+
+        //if our singleton is not a ? we want to transform the player to one of our exit positions
+        if (!MasterData.whereDidIComeFrom.Equals("?"))
+        {
+            if(MasterData.whereDidIComeFrom.Equals("north"))
+            {
+                this.gameObject.transform.position = this.southStart.transform.position;
+                this.rb.AddForce(Vector3.forward * 150.0f);
+            }
+            else if(MasterData.whereDidIComeFrom.Equals("west"))
+            {
+                this.gameObject.transform.position = this.northStart.transform.position;
+                this.rb.AddForce(Vector3.right * 150.0f);
+            }
+            else if(MasterData.whereDidIComeFrom.Equals("east"))
+            {
+                this.gameObject.transform.position = this.westStart.transform.position;
+                this.rb.AddForce(Vector3.right * 150.0f);
+            }
+            else if(MasterData.whereDidIComeFrom.Equals("south"))
+            {
+                this.gameObject.transform.position = this.eastStart.transform.position;
+                this.rb.AddForce(Vector3.back * 150.0f);
+            }
+
+        }
+        print(MasterData.count);
+
     }
 
     // Update is called once per frame
@@ -54,7 +83,7 @@ public class PlayerController : MonoBehaviour
     //this code will load the next scene in the event that the player collides with the exit
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Exit"))
+        if (other.gameObject.CompareTag("Exit") && MasterData.isExiting)
         {
             if (other.gameObject == this.northExit)
             {
@@ -72,7 +101,32 @@ public class PlayerController : MonoBehaviour
             {
                 MasterData.whereDidIComeFrom = "west";
             }
+            MasterData.isExiting = false;
+            SceneManager.LoadScene("DungeonRoom");
+        }
+        else if(other.gameObject.CompareTag("Exit") && !MasterData.isExiting)
+        {
+            MasterData.isExiting = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("center"))
+        {
             SceneManager.LoadScene("DungeonRoom");
         }
     }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("center"))
+        {
+            this.rb.velocity = Vector3.zero;
+            this.rb.isKinematic = true;
+        }
+
+    }*/
 }
+
+
